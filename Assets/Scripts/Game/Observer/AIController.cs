38af5be42a535b5b8caeab819fb1a8cf893fedf8
July Sudarenko <july@sudarenko.ru>
2021-05-20 09:50:56 +0300

    Refactoring

--- Assets/Scripts/Game/Observer/AIController.cs
+++ Assets/Scripts/Game/Observer/AIController.cs
@@ -13,14 +13,6 @@ namespace Game.Observer
 
     public class AIController : BaseController
     {
-        private int _allCountMoneyPlayer;
-        private int _allCountHealthPlayer;
-        private int _allCountForcePlayer;
-
-        private Money _money;
-        private Health _heath;
-        private Force _force;
-
         private Enemy _enemy;
 
         public AIController(Transform placeForUi)
@@ -35,22 +27,6 @@ namespace Game.Observer
             var viewPath = new ResourcePath {PathResource = $"Prefabs/{nameof(ObserverView)}"};
             var view = ResourceLoader.LoadAndInstantiateObject<ObserverView>(viewPath, placeForUi, false);
             view.Init();
-            
-            _money = new Money(nameof(Money));
-            _money.Attach(_enemy);
-
-            _heath = new Health(nameof(Health));
-            _heath.Attach(_enemy);
-
-            _force = new Force(nameof(Force));
-            _force.Attach(_enemy);
-        }
-
-        private void OnDestroy()
-        {
-            _money.Detach(_enemy);
-            _heath.Detach(_enemy);
-            _force.Detach(_enemy);
         }
     }
 }
